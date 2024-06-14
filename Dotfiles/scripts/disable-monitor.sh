aa7bd9f523834553fa6ccaf7ef6b5c5a5069b001
M0streng0 <ho.henrique@proton.me>
2024-06-14 18:37:36 +0200

    changes to the disable-monitor (laptop) script

--- Dotfiles/scripts/disable-monitor.sh
+++ Dotfiles/scripts/disable-monitor.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/bash
 
-# Path to a file where the current state will be stored.
-STATE_FILE="/tmp/monitor_state"
+# Check if the laptop lid is closed by reading the status from /proc/acpi/button/lid/LID/state.
+LID_STATUS=$(grep -c "closed" /proc/acpi/button/lid/LID/state)
 
 # Check how many monitors are connected.
 MONITOR_COUNT=$(hyprctl monitors | grep -c "Monitor")
-MONITOR_NAME=$(hyprctl monitors | grep "Monitor")
 
-# Only proceed if more than one monitor is connected.
-if [ "$MONITOR_COUNT" -gt 1 ] || [[ "$MONITOR_NAME" =~ ID\ 1 ]]; then
-    # Check the current state and toggle the monitor accordingly.
-    if [ -f "$STATE_FILE" ]; then
-        # If the state file exists, re-enable the monitor and delete the state file.
-        hyprctl keyword monitor eDP-1,highres,0x1080,1
-        rm "$STATE_FILE"
-    else
-        # If the state file does not exist, disable the monitor and create the state file.
-        hyprctl keyword monitor eDP-1,disable
-        touch "$STATE_FILE"
-    fi
-else
-    echo "Only one monitor is connected. Exiting script."
+# Only proceed if the lid is closed and more than one monitor is connected.
+if [ "$LID_STATUS" -eq 1 ] && [ "$MONITOR_COUNT" -gt 1 ]; then
+    # Disable the internal monitor.
+    hyprctl keyword monitor eDP-1,disable
+elif [ "$LID_STATUS" -eq 0 ]; then
+    # Enable the internal monitor.
+    hyprctl keyword monitor eDP-1,highres,0x1080,1
 fi
